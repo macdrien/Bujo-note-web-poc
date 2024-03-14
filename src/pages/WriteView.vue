f3f93de81a7fe17471036a9b3622ceceb622c762
macdrien <adrien.bouyssou@gmail.com>
2024-03-14 16:20:08 +0100

    FIX Height bug in mobile

--- src/pages/WriteView.vue
+++ src/pages/WriteView.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
-import { QuillEditor } from '@vueup/vue-quill';
+import { type Quill, QuillEditor } from '@vueup/vue-quill';
+import { type Ref, ref } from 'vue';
+
+const quill: Ref<Quill> = ref();
+
+const onReady = (editor: Quill) => {
+  editor.focus();
+  console.log(editor);
+  quill.value = editor;
+}
+
+const save = () => {
+  console.log(quill.value);
+}
 </script>
 
 <template>
   <div class="write">
-    <h1>Write view</h1>
-    <QuillEditor toolbar="#toolbar" theme="" @ready="(quill) => quill.focus()" />
-      
+    <quill-editor content-type="html" toolbar="#toolbar" theme="" @ready="(quill) => onReady(quill)" />
+
     <div id="toolbar">
       <button class="ql-header" :value="1">T</button>
       <button class="ql-bold">B</button>
@@ -14,28 +26,30 @@ import { QuillEditor } from '@vueup/vue-quill';
       <button class="ql-list" value="bullet">.</button>
       <button class="ql-list" value="ordered">1</button>
       <button class="ql-list" value="check">O</button>
+      <button class="save" @click="save">Save</button>
     </div>
   </div>
 </template>
 
 <style scoped>
 .write {
-  width: 100%;
+  width: 100vw;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 10px 0;
 }
 
 #toolbar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
   border: none;
-  width: 100%;
   display: flex;
   flex-direction: row;
   width: 100%;
-  padding: 1em;
   gap: 0.5em;
+  padding: 5px
 }
 
 #toolbar button {
@@ -49,9 +63,19 @@ import { QuillEditor } from '@vueup/vue-quill';
   font-weight: bold;
 }
 
+#toolbar .save {
+  padding: 2px 5px;
+  border-radius: 5px;
+  width: fit-content;
+}
+
+:deep(.ql-container) {
+  box-sizing: border-box;
+}
+
 :deep(.ql-editor) {
-  width: 100%;
-  min-height: 200px;
+  width: 100vw;
+  height: 100vh;
   text-align: left;
 }
 
