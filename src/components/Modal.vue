742b6e68accb8ba01ac244d026c1289bc4015706
macdrien <adrien.bouyssou@gmail.com>
2024-03-28 14:38:22 +0100

    ADD Create modal to save note

---
+++ src/components/Modal.vue
@@ -0,0 +1,56 @@
+<script setup>
+const emit = defineEmits(['close']);
+</script>
+
+<template>
+  <div class="modal-wrapper">
+    <div class="modal-blur" @click="() => emit('close')"/>
+    <div class="modal">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100vw;
+
+  display: flex;
+  justify-content: center;
+}
+
+.modal-blur {
+  z-index: 1;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: var(--text);
+  opacity: 0.3;
+}
+
+.modal {
+  z-index: 2;
+
+  justify-self: center;
+  align-self: center;
+
+  position: absolute;
+
+  width: 90vw;
+  height: 60vh;
+  max-height: 60vh;
+
+  border-radius: 10px;
+  box-shadow: var(--text) 1px 1px 5px;
+
+  background-color: var(--background);
+}
+</style>
--- src/pages/WriteView.vue
+++ src/pages/WriteView.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-import { type Quill, QuillEditor } from '@vueup/vue-quill';
 import { type Ref, ref } from 'vue';
+import { type Quill, QuillEditor } from '@vueup/vue-quill';
+import Modal from '../components/Modal.vue';
 
 const quill: Ref<Quill> = ref();
+const modalOpen = ref(false);
 
 const onReady = (editor: Quill) => {
   editor.focus();
   quill.value = editor;
 }
 
-const save = () => {
-  console.log(quill.value);
+const editorSave = () => {
+  modalOpen.value = true;
+  console.log(JSON.stringify(quill.value.getContents().ops));
+}
+
+const closeModal = () => {
+  modalOpen.value = false;
 }
 </script>
 
@@ -25,16 +32,63 @@ const save = () => {
       <button class="ql-list" value="bullet">•</button>
       <button class="ql-list" value="ordered">1</button>
       <button class="ql-list" value="check">✓</button>
-      <button class="save" @click="save">Save</button>
+      <button class="save" @click="editorSave">Save</button>
     </div>
   </div>
+
+  <modal v-if="modalOpen" @close="closeModal"> 
+    <div class="save-modal">
+      <h6>Enregistrer la note</h6>
+      <div class="new-note">
+        <p>Nouvelle note :</p>
+        <input type="text"/>
+      </div>
+      <div class="note-list">
+        <p>Ajouter à :</p>
+        <div class="list">
+        <ul>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 1</li>
+          <li>Note 2</li>
+        </ul>
+        </div>
+      </div>
+      <div class="actions">
+        <button>Sauvegarder</button>
+        <button @click="closeModal">Annuler</button>
+      </div>
+    </div>
+  </modal>
 </template>
 
 <style scoped>
 .write {
-  width: calc(100vw - 2px);
-  height: calc(100vh - 2px);
-  padding: 1px;
+  width: calc(100vw - 4px);
+  height: calc(100vh - 4px);
+  padding: 2px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -88,5 +142,46 @@ const save = () => {
 :deep(.ql-container:focus-visible),
 :deep(.ql-container :focus-visible) {
   outline: none;
+}
+
+.save-modal {
+  padding: 0.5em;
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+  height: 100%;
+}
+
+.save-modal h6 {
+  font-size: 1.5em;
+}
+
+.new-note {
+  display: flex;
+  justify-content: space-between;
+}
+
+.note-list {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  gap: 5px;
+  height: 100%;
+  overflow: hidden;
+}
+
+.list {
+  overflow: scroll;
+}
+
+.actions {
+  margin: 0 0.5em 0.5em;
+  display: flex;
+  justify-content: flex-end;
+  gap: 1em;
+}
+
+button {
+  padding: 5px;
 }
 </style>
