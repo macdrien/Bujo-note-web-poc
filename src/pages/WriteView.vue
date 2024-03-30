3c9947e6aaf56b3db4328d8b0bdc625e4dde9417
macdrien <adrien.bouyssou@gmail.com>
2024-03-30 08:20:21 +0100

    DEL Modal

--- src/pages/WriteView.vue
+++ src/pages/WriteView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 import { type Ref, ref } from 'vue';
 import { type Quill, QuillEditor } from '@vueup/vue-quill';
-import Modal from '../components/Modal.vue';
 
 const quill: Ref<Quill> = ref();
-const modalOpen = ref(false);
 
 const onReady = (editor: Quill) => {
   editor.focus();
@@ -12,12 +10,7 @@ const onReady = (editor: Quill) => {
 }
 
 const editorSave = () => {
-  modalOpen.value = true;
   console.log(JSON.stringify(quill.value.getContents().ops));
-}
-
-const closeModal = () => {
-  modalOpen.value = false;
 }
 </script>
 
@@ -35,53 +28,6 @@ const closeModal = () => {
       <button class="save" @click="editorSave">Save</button>
     </div>
   </div>
-
-  <modal v-if="modalOpen" @close="closeModal"> 
-    <div class="save-modal">
-      <h6>Enregistrer la note</h6>
-      <div class="new-note">
-        <p>Nouvelle note :</p>
-        <input type="text"/>
-      </div>
-      <div class="note-list">
-        <p>Ajouter à :</p>
-        <div class="list">
-        <ul>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-          <li>Note 1</li>
-          <li>Note 2</li>
-        </ul>
-        </div>
-      </div>
-      <div class="actions">
-        <button>Sauvegarder</button>
-        <button @click="closeModal">Annuler</button>
-      </div>
-    </div>
-  </modal>
 </template>
 
 <style scoped>
@@ -142,46 +88,5 @@ const closeModal = () => {
 :deep(.ql-container:focus-visible),
 :deep(.ql-container :focus-visible) {
   outline: none;
-}
-
-.save-modal {
-  padding: 0.5em;
-  display: flex;
-  flex-direction: column;
-  gap: 1em;
-  height: 100%;
-}
-
-.save-modal h6 {
-  font-size: 1.5em;
-}
-
-.new-note {
-  display: flex;
-  justify-content: space-between;
-}
-
-.note-list {
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  gap: 5px;
-  height: 100%;
-  overflow: hidden;
-}
-
-.list {
-  overflow: scroll;
-}
-
-.actions {
-  margin: 0 0.5em 0.5em;
-  display: flex;
-  justify-content: flex-end;
-  gap: 1em;
-}
-
-button {
-  padding: 5px;
 }
 </style>
