b7ff68f6c8d9579a9d75c8e02aaeb2996c7dc4e4
macdrien <adrien.bouyssou@gmail.com>
2024-04-04 14:18:36 +0200

    ADD Navigate to save view on save

---
+++ src/pages/SaveView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Delta, QuillEditor } from '@vueup/vue-quill';
+import { ref } from 'vue';
+
+const props = defineProps({
+  note: {
+    type: Delta,
+    required: true,
+  },
+});
+
+const noteToSave = ref<Delta>(props.note);
+</script>
+
+<template>
+  <quill-editor v-model:content="noteToSave" content-type="delta" theme="" :enable="false" :read-only="true" :toolbar="undefined" />
+</template>
+
+<style scoped>
+:deep(.ql-clipboard) {
+  display: none;
+}
+</style>
