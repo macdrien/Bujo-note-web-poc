d604f01a1fe76623f52c56166bfa520c7bb6d9f9
macdrien <adrien.bouyssou@gmail.com>
2024-04-09 14:03:27 +0200

    ADD Login page

---
+++ src/pages/LoginView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const identifier = ref<String>('');
+const password = ref<String>('');
+</script>
+
+<template>
+  <div class="loginView">
+    <h1>Se connecter</h1>
+    <div class="form">
+      <div class="formField">
+        <p>Pseudo / Email :</p>
+        <input v-model="identifier" type="text" required />
+      </div>
+      <div class="formField">
+        <p>Mot de passe :</p>
+        <input v-model="password" type="text" required />
+      </div>
+      <button class="submit">Se connecter</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loginView {
+  height: 100vh;
+  width: 100vw;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/pages/WelcomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <h1>Welcome view</h1>
-  <RouterLink to="/write">Write</RouterLink>
-  <RouterLink to="/list">List</RouterLink>
-</template>
