8066d49d941e2c0effe960dd16174f9e4ef4dc88
macdrien <adrien.bouyssou@gmail.com>
2024-04-09 14:03:27 +0200

    ADD Login page

--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,15 @@
 import { createWebHashHistory, createRouter } from 'vue-router';
-import WelcomeView from './pages/WelcomeView.vue';
 import WriteView from './pages/WriteView.vue';
 import ListView from './pages/ListView.vue';
 import SaveView from './pages/SaveView.vue';
+import LoginView from './pages/LoginView.vue';
 import { Delta } from '@vueup/vue-quill';
 
 const routes = [
   { path: '/notes', component: ListView },
   { path: '/write', component: WriteView },
   { path: '/save', component: SaveView, props: (route: any) => ({ note: new Delta(JSON.parse(decodeURIComponent(route.query.note))) }) },
-  { path: '/', component: WelcomeView },
+  { path: '/', component: LoginView },
 ];
 
 export const router = createRouter({
