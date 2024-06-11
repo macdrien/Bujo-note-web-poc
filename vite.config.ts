b56f81eb327b6cc5aaf8bac7e56ca2d22e4b6dda
Adrien Bouyssou <adrien.bouyssou@gmail.com>
2024-06-11 08:47:41 +0000

    UPD Website URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bujo-note/",
+  base: "/Bujo-note-web-poc/",
   plugins: [vue()],
   optimizeDeps: {
     exclude: ["oh-vue-icons/icons"]
